1088fbfa63104c7519c3a385290ede4d58234cc2
ngohuy0972 <huyngoit99@gmail.com>
2022-05-17 21:33:08 +0700

    improve UI login, register and profile screen, refs#master

--- lib/application.dart
+++ lib/application.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
                 future: _initializeFirebase(),
                 builder: ((context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.done) {
-                    return Login();
+                    return const Login();
                   }
                   return const Center(
                     child: CircularProgressIndicator(),
--- lib/containers/authentications/login.dart
+++ lib/containers/authentications/login.dart
@@ -66,119 +66,112 @@ class _LoginState extends State<Login> {
     TextEditingController _emailController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
 
-    var logoLogin = Expanded(
-        flex: 2,
-        child: Image.asset(
-          'instagram_logo.png',
-          width: 200,
-          fit: BoxFit.contain,
-        ));
+    var logoLogin = Image.asset(
+      'instagram_logo.png',
+      height: 50,
+    );
 
-    var inputComponent = Expanded(
-        flex: 4,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "You can't have an empty email!";
-                }
+    var inputComponent = Column(
+      children: <Widget>[
+        TextFormField(
+          controller: _emailController,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "You can't have an empty email!";
+            }
 
-                if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                  return "Please enter a valid email address";
-                }
+            if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+              return "Please enter a valid email address";
+            }
 
-                if (value.length > 50) {
-                  return "Email does not exceed 50 characters!";
-                }
-              },
-              decoration: const InputDecoration(labelText: 'Email Address'),
-              keyboardType: TextInputType.emailAddress,
-              maxLength: 50,
-            ),
-            TextFormField(
-              controller: _passwordController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "You can't have an empty password!";
-                }
+            if (value.length > 50) {
+              return "Email does not exceed 50 characters!";
+            }
+          },
+          decoration: const InputDecoration(labelText: 'Email Address'),
+          keyboardType: TextInputType.emailAddress,
+          maxLength: 50,
+        ),
+        TextFormField(
+          controller: _passwordController,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "You can't have an empty password!";
+            }
 
-                if (!RegExp(
-                        r'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9_])')
-                    .hasMatch(value)) {
-                  return "Password includes uppercases, special characters";
-                }
+            if (!RegExp(r'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9_])')
+                .hasMatch(value)) {
+              return "Password includes uppercases, special characters";
+            }
 
-                if (value.length < 6) {
-                  return "Password must be least 6 characters";
-                }
+            if (value.length < 6) {
+              return "Password must be least 6 characters";
+            }
 
-                if (value.length > 20) {
-                  return "Password does not exceed 20 characters!";
-                }
-              },
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              maxLength: 20,
-            ),
-          ],
-        ));
+            if (value.length > 20) {
+              return "Password does not exceed 20 characters!";
+            }
+          },
+          decoration: const InputDecoration(labelText: 'Password'),
+          obscureText: true,
+          maxLength: 20,
+        ),
+      ],
+    );
 
-    var buttonSubmit = Expanded(
-        flex: 4,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50)),
-            onPressed: () async {
-              _formKey.currentState!.validate();
-              User? user = await loginUsingEmailPassword(
-                  email: _emailController.text,
-                  password: _passwordController.text,
-                  context: context);
-              if (user != null) {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyStatefulWidget()));
-                Future.delayed(const Duration(milliseconds: 1000), () {
-                  successDialog();
-                });
-              } else {
-                // failedDialog();
-              }
-            },
-            child: const Text(
-              'Sign In',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-            ),
-          ),
-          TextButton(
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50)),
-            onPressed: () => {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Register()))
-            },
-            child: const Text(
-              'Sign Up',
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            ),
-          )
-        ]));
+    var buttonSubmit =
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+        onPressed: () async {
+          _formKey.currentState!.validate();
+          User? user = await loginUsingEmailPassword(
+              email: _emailController.text,
+              password: _passwordController.text,
+              context: context);
+          if (user != null) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyStatefulWidget()));
+            Future.delayed(const Duration(milliseconds: 1000), () {
+              successDialog();
+            });
+          } else {
+            // failedDialog();
+          }
+        },
+        child: const Text(
+          'Sign In',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+        ),
+      ),
+      TextButton(
+        style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+        onPressed: () => {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const Register()))
+        },
+        child: const Text(
+          'Sign Up',
+          style: TextStyle(
+              color: Colors.blue, fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+      )
+    ]);
 
-    return Material(
-        child: Container(
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Container(
             margin: const EdgeInsets.all(20.0), // Or set whatever you want
             child: SafeArea(
               child: Form(
                   key: _formKey,
-                  child: Column(
+                  child: ListView(
                     children: <Widget>[
+                      const SizedBox(
+                        height: 100,
+                      ),
                       logoLogin,
                       inputComponent,
                       buttonSubmit,
